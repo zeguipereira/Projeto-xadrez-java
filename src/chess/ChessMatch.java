24e92594b84d6a133d80d5e8639a9fd01202b931
zeguipereira <161784864+zeguipereira@users.noreply.github.com>
2026-04-10 14:33:39 -0300

    Colocando as primeiras peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,10 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +12,7 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -19,5 +24,11 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
     }
 }
